636aa016b99839c1fe83ce28f623d6a80c33e6e9
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-11-15 17:04:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// let num = "124";
+// document.write(num + 1 , "<br>");
+
+// document.write(Number (num) + 1);
+
+// document.write(parent("123"));
+
+// document.write(Number("123"));
+
+// document.write(new Date());
+
+// document.write(new Date("123 654"));
+
+// console.log(new Date("123 654"));
+
+
+// Number
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(new Date()));
+
+// console.log(Number(999));
+// console.log(Number("999"));
+// console.log(Number("999 888"));
+
+// document.write(Number([8])); // retrun same 8.
+// document.write(Number([8.9])); // retrun same (8.9).
+// document.write(Number([8 , 9]));// retrun Not a Num.
+// console.log(Number("469643436484"));
+
+// //To Fix:
+// let decimal = 45.63836556984;
+// console.log(decimal); // same 45.63836556984
+// console.log(decimal.toFixed()); // round up 45.63836556984 = 45
+// console.log(decimal.toFixed(5)); // 45.63836556984 = 45.63836
+
+// let num = 23.46246298
+// console.log(num);
+// console.log(num.toFixed());
+// console.log(num.toFixed(5));
+
+
+
+
